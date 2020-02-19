3af83e356c003d94eebb6233257a55209a40b70a
lena <oleshko.lena@gmail.com>
2020-02-19 15:50:14 +0100

    Centered the content of each pizza item

--- src/about.js
+++ src/about.js
@@ -11,13 +11,13 @@ function about() {
     text.setAttribute("class", "text-field");
     text.innerText = `Since 1994, Ristorante Da Angelo has been 
     bringing authentic Italian food to Tomsk.
-    
+    \b\r
     We offer the menu of tried and true favourites, 
     mainly from the southern regions of Italy, 
     where pizza originated and where pasta is king.
     In true Italian fashion just about everything 
     is freshly made on the premises.
-    
+    \b\r
     And if you want to know why the gnocchi, 
     in particular, tastes so exceptional — 
     it’s because Angelo’s and Marco’s mothers, 
